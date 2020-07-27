50df4217f90c66e82a4f9e8b4076159b970acecf
Mayuri0612 <500060750@stu.upes.ac.in>
2020-07-27 20:46:34 +0530

    Day1 in 30 days of python

---
+++ Day1_Basics.py
@@ -0,0 +1,6 @@
+#download python from "https://www.python.org/ftp/python/3.8.5/python-3.8.5.exe"
+#all information about release notes "https://www.python.org/"
+#First basic command
+
+x = 10*10
+print(x)
