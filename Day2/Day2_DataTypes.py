4dc514a0feb7e6f72b46cd19c2f5439312560824
Mayuri0612 <500060750@stu.upes.ac.in>
2020-07-30 12:42:18 +0530

    30 days of python series

---
+++ Day2/Day2_DataTypes.py
@@ -0,0 +1,18 @@
+# This file contains knowledge about python number variables and strings.
+
+# Some basics maths in python
+#python serves numbers as numbers only
+x = 10*10
+print(x)
+
+x = 10/345
+print(x) #gives decimal value
+
+import math
+print(math.floor(10/345)) #gives rounded off values
+print(math.floor(1000/345))
+
+
+#Strings in python
+print("hello" * 3 ) #python can multiply string and numbers and repeat it that number of times.
+print('hello world' + ' ' + 'another') #string concatenation
